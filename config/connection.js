1e8ddf3c694ab00db6cb71fb8e88f7b5c25680d5
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-08 13:18:51 -0500

    Fixed connection issues with localhost and used IP instead

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://localhost/socialNetwork_DB', {
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = connection;
+// Export connection 
+module.exports = mongoose.connection;
